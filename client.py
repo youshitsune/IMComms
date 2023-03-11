8a0acf4c23655dde2eafb6488cf8c2ac45f802ee
youshitsune <luka@youshitsune.me>
2023-03-11 11:47:59 +0100

    Implement RSA encryption with 4096 size keys

--- client.py
+++ client.py
@@ -1,14 +1,33 @@
+import threading
 import socket
+import rsa
+import os
+
+if os.path.exists("private_key.pem") and os.path.exists("public_key.pem"):
+    pub_key = rsa.PublicKey.load_pkcs1(open("public_key.pem", "rb").read())
+    priv_key = rsa.PrivateKey.load_pkcs1(open("private_key.pem", "rb").read())
+
+else:
+    pub_key, priv_key = rsa.newkeys(4096)
+pub_con = None
 
 target_host = "127.0.0.1"
 target_port = 9998
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((target_host, target_port))
-while True:
-    msg = input("Message: ")
-    client.send(msg.encode('utf-8'))
-    response = client.recv(4096)
-    print(f"[*] Message: {response.decode('utf-8')}")
+client.send(pub_key.save_pkcs1("PEM"))
+pub_con = rsa.PublicKey.load_pkcs1(client.recv(4096))
 
-client.close()
+def send_msg(client):
+    while True:
+        msg = input("You: ")
+        client.send(rsa.encrypt(msg.encode("utf-8"), pub_con))
+
+def recv_msg(client):
+    while True:
+        print(f"Contact: {rsa.decrypt(client.recv(4096), priv_key).decode('utf-8')}")
+
+threading.Thread(target=send_msg, args=(client,)).start()
+threading.Thread(target=recv_msg, args=(client, )).start()
+
--- server.py
+++ server.py
@@ -1,20 +1,36 @@
+import threading
 import socket
+import rsa
+import os
+
+if os.path.exists("private_key.pem") and os.path.exists("public_key.pem"):
+    pub_key = rsa.PublicKey.load_pkcs1(open("public_key.pem", "rb").read())
+    priv_key = rsa.PrivateKey.load_pkcs1(open("private_key.pem", "rb").read())
+else:
+    pub_key, priv_key = rsa.newkeys(4096)
+
+pub_con = None
 
 IP = "0.0.0.0"
 PORT = 9998
 
-def main():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((IP, PORT))
-    server.listen(5)
-    print(f"[*] Listening on {IP}:{PORT}")
-    client, address = server.accept()
-    while True:
-        request = client.recv(4096)
-        print(f"[*] Message: {request.decode('utf-8')}")
-        client.send(input().encode("utf-8"))
-        client.send(b"ACK")
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((IP, PORT))
+server.listen()
+print(f"[*] Listening on {IP}:{PORT}")
+client, address = server.accept()
+pub_con = rsa.PublicKey.load_pkcs1(client.recv(4096))
+client.send(pub_key.save_pkcs1("PEM"))
 
-if __name__ == "__main__":
-    main()
+def send_msg(client):
+    while True:
+        msg = input("You: ")
+        client.send(rsa.encrypt(msg.encode("utf-8"), pub_con))
+
+def recv_msg(client):
+    while True:
+        print(f"Contact: {rsa.decrypt(client.recv(4096), priv_key).decode('utf-8')}")
+
+threading.Thread(target=send_msg, args=(client, )).start()
+threading.Thread(target=recv_msg, args=(client, )).start()
     
